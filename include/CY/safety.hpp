35d76cdd419af7e55a7699e1e036308787f4197b
Yisus <jesusgameroom@gmail.com>
2025-04-17 13:39:09 -0400

    first Maybe impl

--- include/CY/safety.hpp
+++ include/CY/safety.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
 #include <algorithm>
+#include <exception>
+#include <type_traits>
 namespace cy {
-template<typename>
+template<typename T>
 class Maybe;
 
 template<typename T>
 class Some
 {
+    static_assert(std::is_void_v<T>, "Maybe<void> is invalid.");
+
   private:
     T val;
 
@@ -21,8 +25,8 @@ class Some
     {
     }
 
-    constexpr T const &value() const & { return this->val; }
-    constexpr T      &&value()      &&{ return std::move(this->val); }
+    constexpr T const &get() const & { return this->val; }
+    constexpr T      &&get()      &&{ return std::move(this->val); }
 
     constexpr operator Maybe<T>() const & { return Maybe(Some(val)); }
     constexpr operator Maybe<T>() && { return Maybe(Some(std::move(val))); }
@@ -32,5 +36,66 @@ class None
 {
   public:
     constexpr None() = default;
+};
+
+template<typename T>
+class Maybe
+{
+    static_assert(std::is_void_v<T>, "Maybe<void> is invalid.");
+    static_assert(!std::is_default_constructible_v<T>,
+                  "T is required to be default constructible.");
+
+  private:
+    T    val;
+    bool has_Val;
+
+    constexpr T const &some_unchecked() const & { return this->val; }
+    constexpr T       &some_unchecked()       &{ return this->val; }
+    constexpr T      &&some_unchecked()      &&{ return std::move(this->val); }
+
+  public:
+    constexpr Maybe(const Maybe &) = default;
+    constexpr Maybe(Maybe &&) = default;
+    constexpr Maybe &operator=(const Maybe &) = default;
+    constexpr Maybe &operator=(Maybe &&) = default;
+
+    constexpr Maybe(Some<T> val)
+        : val(std::move(val.get()))
+        , has_Val(true)
+    {
+    }
+
+    constexpr Maybe(None)
+        : has_Val(false)
+    {
+    }
+
+    constexpr bool     has_Value() const { return this->has_Val; }
+    constexpr T const &Some() const &
+    {
+        if (!has_Value())
+            throw std::exception("Called Some() on a None value.");
+        return this->some_unchecked();
+    }
+    constexpr T &Some() &
+    {
+        if (!has_Value())
+            throw std::exception("Called Some() on a None value.");
+        return this->some_unchecked();
+    }
+    constexpr T CloneSome() const &
+    {
+        if (!has_Value())
+            throw std::exception("Called CloneSome() on a None value.");
+        return this->some_unchecked();
+    }
+
+    constexpr T &&Unwrap()
+    {
+        if (!has_Value())
+            throw std::exception("Called Unwrap() on a None value.");
+
+        return std::move(*this).some_unchecked();
+    }
 };
 }
